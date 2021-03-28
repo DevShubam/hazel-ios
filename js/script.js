00569f8d3cc4d87d3681aeae13999da1ff9adc88
Shubam Sharma <65375709+DevShubam@users.noreply.github.com>
2021-03-28 15:00:39 -0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+///////////////////////////////////////////
+//             Dropdown Menu             //
+///////////////////////////////////////////
+// Opens / Closes the menu
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+///////////////////////////////////////////
+//           END Dropdown Menu           //
+///////////////////////////////////////////
